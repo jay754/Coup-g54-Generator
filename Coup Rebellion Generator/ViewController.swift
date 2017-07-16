47968f1b44650b2091292715e525e4d8fed021a9
Jay Engineer <jayengineer6@gmail.com>
2017-07-15 22:39:43 -0400

    i can't design

--- Coup Rebellion Generator/ViewController.swift
+++ Coup Rebellion Generator/ViewController.swift
@@ -10,25 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // The Title UI labels
+    @IBOutlet weak var communicationsTitle: UILabel!
+    @IBOutlet weak var financeTitle: UILabel!
+    @IBOutlet weak var forceTitle: UILabel!
+    @IBOutlet weak var sp1Title: UILabel!
+    @IBOutlet weak var sp2Title: UILabel!
+    
+    // The ability UI labels
     @IBOutlet weak var communicationsAbility: UILabel!
     @IBOutlet weak var financeAbility: UILabel!
     @IBOutlet weak var forceAbility: UILabel!
     @IBOutlet weak var sp1Ability: UILabel!
     @IBOutlet weak var sp2Ability: UILabel!
     
+    // Initiate for data
     let C = Communications()
     let F = Finance()
     let Fo = Force()
     let sp1 = specialInterests()
     let sp2 = specialInterests()
 
+    // onload method
     override func viewDidLoad() {
         super.viewDidLoad()
         
         selectItems()
     }
     
-    func rand() -> (cRoll: Int, fiRoll: Int, foRoll: Int, sp1: Int, sp2: Int){
+    //random num generator -> returns a tuple
+    func rand() -> (cRoll: Int, fiRoll: Int, foRoll: Int, sp1: Int, sp2: Int) {
         let cRoll = Int(arc4random_uniform(UInt32(4)))
         let fiRoll = Int(arc4random_uniform(UInt32(5)))
         let foRoll = Int(arc4random_uniform(UInt32(5)))
@@ -42,19 +53,35 @@ class ViewController: UIViewController {
         
         return (cRoll, fiRoll, foRoll, sp1, sp2)
     }
-
-    func selectItems(){
-        let r = rand()
-        
-        communicationsAbility.text = String(C.List[r.0]["Ability"]!)
-        financeAbility.text = String(F.List[r.1]["Ability"]!)
-        forceAbility.text = String(Fo.List[r.2]["Ability"]!)
-        sp1Ability.text = String(sp1.List[r.3]["Ability"]!)
-        sp2Ability.text = String(sp2.List[r.4]["Ability"]!)
+    
+    // resizing the label to fit text
+    func rSize(lab: UILabel, content: String) {
+        lab.text = content
+        lab.adjustsFontSizeToFitWidth = true
+        lab.sizeToFit()
+        lab.textAlignment = .center
     }
     
-    @IBAction func generate(){
+    // click button action
+    @IBAction func generate() {
         selectItems()
+    }
+    
+    // for getting the text and the data
+    func selectItems() {
+        let r = rand()
+        
+        rSize(lab: communicationsTitle, content: String(C.List[r.0]["Title"]!))
+        rSize(lab: financeTitle, content: String(F.List[r.1]["Title"]!))
+        rSize(lab: forceTitle, content: String(Fo.List[r.2]["Title"]!))
+        rSize(lab: sp1Title, content: String(sp1.List[r.3]["Title"]!))
+        rSize(lab: sp2Title, content: String(sp2.List[r.4]["Title"]!))
+        
+        rSize(lab: communicationsAbility, content: String(C.List[r.0]["Ability"]!))
+        rSize(lab: financeAbility, content: String(F.List[r.1]["Ability"]!))
+        rSize(lab: forceAbility, content: String(Fo.List[r.2]["Ability"]!))
+        rSize(lab: sp1Ability, content: String(sp1.List[r.3]["Ability"]!))
+        rSize(lab: sp2Ability, content: String(sp2.List[r.4]["Ability"]!))
     }
     
 }
